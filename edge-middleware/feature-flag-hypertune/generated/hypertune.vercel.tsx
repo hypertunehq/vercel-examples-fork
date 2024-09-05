b8de7ca75a75c1dd0f3553505becd3e78900957f
Michal Bock <michal.bock@gmail.com>
2024-09-05 16:56:56 +0100

    feat: update dependencies and rerun codegen to support nested flags

--- edge-middleware/feature-flag-hypertune/generated/hypertune.vercel.tsx
+++ edge-middleware/feature-flag-hypertune/generated/hypertune.vercel.tsx
@@ -4,19 +4,33 @@ import { type FlagOverridesType, encrypt, decrypt } from '@vercel/flags'
 import { FlagValues } from '@vercel/flags/react'
 import { cookies } from 'next/headers'
 import { unstable_flag as flag } from '@vercel/flags/next'
+import { DeepPartial, ObjectValue, Value } from 'hypertune'
 import * as hypertuneTypes from './hypertune'
 import getHypertune from '../lib/getHypertune'
 
-export async function getVercelOverride(): Promise<hypertuneTypes.Source | null> {
+export async function getVercelOverride(): Promise<DeepPartial<hypertuneTypes.Source> | null> {
   const overridesCookieValue = cookies().get('vercel-flag-overrides')?.value
 
   if (!overridesCookieValue) {
     return null
   }
 
-  const root = (await decrypt<FlagOverridesType>(
+  const decryptedOverrides = await decrypt<FlagOverridesType>(
     overridesCookieValue
-  )) as hypertuneTypes.Root
+  )
+  if (!decryptedOverrides) {
+    return null
+  }
+
+  const root = Object.fromEntries(
+    Object.entries(decryptedOverrides).map(([flagPath, value]) => {
+      const [flagName, ...flagPathSteps] = flagPath.split('.')
+      return [
+        flagName,
+        flagPathSteps.reduce((current, step) => ({ [step]: current }), value),
+      ]
+    })
+  ) as DeepPartial<hypertuneTypes.Root>
 
   return { root }
 }
@@ -26,13 +40,28 @@ export async function VercelFlagValues({
 }: {
   flagValues: hypertuneTypes.Root
 }): Promise<React.ReactElement> {
-  const filteredFlagValues = Object.fromEntries(
-    Object.entries(flagValues).filter(
-      ([flagKey, flagValue]) => !flagKey.startsWith('__')
-    )
+  const flattenedFlagValues = Object.fromEntries(
+    getVercelFlagValuesEntries('', flagValues)
   )
-  const encryptedFlagValues = await encrypt(filteredFlagValues)
+
+  const encryptedFlagValues = await encrypt(flattenedFlagValues)
   return <FlagValues values={encryptedFlagValues} />
+}
+
+function getVercelFlagValuesEntries(
+  keyPrefix: string,
+  sourceObject: ObjectValue
+): [string, Value][] {
+  return Object.entries(sourceObject).flatMap(([flagKey, flagValue]) => {
+    if (flagKey.startsWith('__') || Array.isArray(flagValue)) {
+      return []
+    }
+
+    if (typeof flagValue !== 'object') {
+      return [[`${keyPrefix}${flagKey}`, flagValue]]
+    }
+    return getVercelFlagValuesEntries(`${keyPrefix}${flagKey}.`, flagValue)
+  })
 }
 
 export const exampleFlagFlag = flag<boolean>({
